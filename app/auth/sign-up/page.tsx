4fdc57fded3ad253d5686a20de8649c5722ea7f4
Lucas Silva dos Santos <lucassilvadossantos2005@gmail.com>
2026-02-13 16:08:32 -0300

    nao precisa de autorizar email

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -42,17 +42,12 @@ export default function SignUpPage() {
       const { data, error } = await supabase.auth.signUp({
         email,
         password,
-        options: {
-          emailRedirectTo:
-            process.env.NEXT_PUBLIC_DEV_SUPABASE_REDIRECT_URL ||
-            `${window.location.origin}/dashboard`,
-        },
       })
       if (error) throw error
       if (data.session) {
         router.push("/dashboard")
       } else {
-        router.push("/auth/sign-up-success")
+        router.push("/dashboard")
       }
     } catch (error: unknown) {
       setError(
